837146a81951296b66a50345aa475639d88a722e
PhilipBrodovsky <philipbrodovsky@gmail.com>
2022-07-14 01:24:24 +0300

    i

--- next-home/pages/index.tsx
+++ next-home/pages/index.tsx
@@ -1,20 +1,24 @@
 import type { NextPage } from "next";
 import styles from "../styles/Home.module.css";
 
+console.log(styles);
+
 const Home: NextPage = () => {
-    return (
-        <div className={styles.container}>
-            <h1>header</h1>
-        </div>
-    );
+	return (
+		<div className="">
+			<div className={styles.appBar}>
+				<span>dasda</span>
+			</div>
+		</div>
+	);
 };
 
 export const getStaticProps = () => {
-    return {
-        props: {
-            buildTimestamp: Date.now(),
-        },
-    };
+	return {
+		props: {
+			buildTimestamp: Date.now(),
+		},
+	};
 };
 
 export default Home;
