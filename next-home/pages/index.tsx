9cbd70e4251e9001d9c50d188ab82bf1dda3551e
PHILBRO <philip@clawdia.ai>
2022-07-14 09:17:37 +0300

    pus

--- next-home/pages/index.tsx
+++ next-home/pages/index.tsx
@@ -1,24 +1,32 @@
 import type { NextPage } from "next";
+import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 console.log(styles);
 
 const Home: NextPage = () => {
-	return (
-		<div className="">
-			<div className={styles.appBar}>
-				<span>dasda</span>
-			</div>
-		</div>
-	);
+    return (
+        <div className="">
+            <div className={styles.appBar}>
+                <Image src={"/images/logo.png"} width={75} height={75} />
+                <nav className={styles.nav}>
+                    <div className="link">products</div>
+                    <div className="link">pricing</div>
+                    <div className="link">pricing</div>
+                    <div className="link">pricing</div>
+                </nav>
+                <button className={styles.button}>Sign in</button>
+            </div>
+        </div>
+    );
 };
 
 export const getStaticProps = () => {
-	return {
-		props: {
-			buildTimestamp: Date.now(),
-		},
-	};
+    return {
+        props: {
+            buildTimestamp: Date.now(),
+        },
+    };
 };
 
 export default Home;
