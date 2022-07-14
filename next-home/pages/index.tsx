ae7f6477636f2019ea4211568dd238de1917e86d
PhilipBrodovsky <philipbrodovsky@gmail.com>
2022-07-14 22:36:07 +0300

    next

--- next-home/pages/index.tsx
+++ next-home/pages/index.tsx
@@ -2,31 +2,33 @@ import type { NextPage } from "next";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
+import logo from "../images/logo.png";
+
 console.log(styles);
 
 const Home: NextPage = () => {
-    return (
-        <div className="">
-            <div className={styles.appBar}>
-                <Image src={"/images/logo.png"} width={75} height={75} />
-                <nav className={styles.nav}>
-                    <div className="link">products</div>
-                    <div className="link">pricing</div>
-                    <div className="link">pricing</div>
-                    <div className="link">pricing</div>
-                </nav>
-                <button className={styles.button}>Sign in</button>
-            </div>
-        </div>
-    );
+	return (
+		<div className="">
+			<div className={styles.appBar}>
+				<Image src={logo} alt="" />
+				<nav className={styles.nav}>
+					<div className="link">products</div>
+					<div className="link">pricing</div>
+					<div className="link">pricing</div>
+					<div className="link">pricing</div>
+				</nav>
+				<button className={styles.button}>Sign in</button>
+			</div>
+		</div>
+	);
 };
 
 export const getStaticProps = () => {
-    return {
-        props: {
-            buildTimestamp: Date.now(),
-        },
-    };
+	return {
+		props: {
+			buildTimestamp: Date.now(),
+		},
+	};
 };
 
 export default Home;
