a2c3b9d86f7a35067d2d1ff1a159c32b166a8e08
PhilipBrodovsky <philipbrodovsky@gmail.com>
2022-07-18 17:19:55 +0300

    refactor home page

--- client-soccer-manager/src/pages/HomePage.tsx
+++ client-soccer-manager/src/pages/HomePage.tsx
@@ -1,12 +1,24 @@
-import { Box, Stack, Typography } from "@mui/material";
+import { Box, Button, Stack, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 export const HomePage = () => {
+	const navigate = useNavigate();
 	return (
-		<Stack sx={{ margin: "0 auto" }}>
-			<Typography variant="h3">Manage Your Dream Team</Typography>
-			<Typography variant="body1">
-				Create groups, players, assign players to group and start play.
-			</Typography>
+		<Stack sx={{ margin: "0 auto", p: 2 }} gap={2}>
+			<Typography variant="h5">Manage Your Dream Team</Typography>
+			<Typography variant="h6">how to start?</Typography>
+			<Typography variant="body1">1) add some players</Typography>
+			<Typography variant="body1">2) create group</Typography>
+			<Typography variant="body1">3) in your group create new cycle</Typography>
+			<Typography variant="body1">3) in your cycle create new game</Typography>
+			<Typography variant="body1">3) start the game</Typography>
+
+			<Button onClick={() => navigate("/players/create")} variant="contained">
+				Add Players
+			</Button>
+			<Button onClick={() => navigate("/groups/create")} variant="contained">
+				Add Group
+			</Button>
 		</Stack>
 	);
 };
